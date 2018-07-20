6ab34e5ea25523fe80a0ec41ef7aceecf89472ad
Patric Boscolo <patbosc@microsoft.com>
2018-07-20 10:03:12 +0000

    Added Tests

---
+++ test/Memento.Test/Memento.Test/MementoTest.cs
@@ -0,0 +1,14 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Memento.Test
+{
+    [TestClass]
+    public class MementoTest
+    {
+        [TestMethod]
+        public void ShouldReturnTrue()
+        {
+            Assert.IsTrue(true);
+        }
+    }
+}
